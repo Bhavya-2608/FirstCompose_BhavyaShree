63f8549fe8e9b66410072babbd135d2409dc4807
Bhavya <sreedbhavya@gmail.com>
2026-02-21 19:37:24 +0530

    Add files via upload

---
+++ main/java/com/example/firstcompose_bhavyashree/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.firstcompose_bhavyashree
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.firstcompose_bhavyashree.ui.theme.FirstCompose_BhavyaShreeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FirstCompose_BhavyaShreeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstCompose_BhavyaShreeTheme {
+        Greeting("Android")
+    }
+}
